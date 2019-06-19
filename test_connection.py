3c7edb0ed9f6d7382064678d1d152a0178ca3cfd
kim <skok1025@naver.com>
2019-06-19 13:48:54 +0900

    Initial commit

---
+++ test_connection.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        user='webdb',
+        password='webdb',
+        host='192.168.1.250',
+        port='5432',
+        database='webdb')
+except Exception as e:
+    print(f'error: {e}')
+finally:
+    'conn' in locals() and conn and conn.close()
